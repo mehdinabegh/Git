e2201a3571be0688da328167c79b1d8984b77af2
mehdinabegh <40233055+mehdinabegh@users.noreply.github.com>
2022-03-26 12:20:18 +0430

    add

---
+++ c#/Class_Person/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Person
+{
+    internal class Person  
+    {
+        public Person()
+        {
+
+        }
+        public Person(int id)
+        {
+            this.ID = id;
+        }
+        public Person(int id,string name , string family):this(id)
+        {
+            this.Name = name;
+            this.Family= family;
+        }
+        private string _name;
+        private string _family;
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Family { get { return _family; } set { _family = value; } }
+        public int ID { get; set; }
+        public string ShowInfo { get { return string.Format("{0}\r\n{1}\r\n{2}", this.Name, this.Family, this.ID); } }
+    }
+}
---
+++ c#/Class_Person/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Class_Person.Person p1=new Class_Person.Person();
+p1.Name = "ali";
+p1.Family = "alavi";
+p1.ID = 1;
+//p1.ShowInfo(p1.Name,p1.Family,p1.ID.ToString());
+Console.WriteLine(p1.ShowInfo);
