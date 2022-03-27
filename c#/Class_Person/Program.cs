41f1c9fc85d6374d9a3202a0249b7db532220082
mehdinabegh <mehdi_nabegh@yahoo.com>
2022-03-27 21:08:12 +0430

    Change at home

--- c#/Class_Person/Program.cs
+++ c#/Class_Person/Program.cs
@@ -6,5 +6,9 @@ p1.Name = "ali";
 p1.Family = "alavi";
 p1.ID = 1;
 //p1.ShowInfo(p1.Name,p1.Family,p1.ID.ToString());
-Console.WriteLine(p1.ShowInfo);
+Console.WriteLine(p1.ShowInfo); 
+Console.WriteLine("hello");
+
+
+
 
